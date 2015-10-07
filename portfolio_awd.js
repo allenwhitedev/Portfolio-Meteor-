1b982db4dac5ff9eb946d225094630c6808d15eb
allenwhitedev <allenwhitedev@gmail.com>
2015-10-06 22:05:19 -0400

    Quick reformat of html layout

--- portfolio_awd.js
+++ portfolio_awd.js
@@ -5,9 +5,18 @@ if (Meteor.isClient)
 	'click .closeButton': function()
 	{
 		console.log("clicked close")
-		$('.browserWrapper').addClass('fadeOutLeft')
+		$('.browserWrapper').addClass('zoomOut')
 		Meteor.setTimeout(function(){$('.browserWrapper').addClass('displayNone')}, 
 			1200)
+	}
+	,
+	'click #prevLVD': function()
+	{
+		console.loge('prev lvd')
+	},
+	'click #nextLVD': function()
+	{
+		console.log('next lvd')
 	}
 })
 
